35068b5846c295693c7a8a561ce416f01a4ecf3a
muntaxir4 <mjmullick0608@gmail.com>
2025-03-26 05:28:50 +0530

    move vite js to vite typescript

---
+++ Frontend/src/components/ui/button.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { cn } from "../../lib/utils";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default: "bg-gray-50 text-gray-600 hover:bg-gray-300",
+        destructive: "bg-destructive text-gray-600 hover:bg-gray-50",
+        outline:
+          "border border-gray-200 bg-white hover:bg-gray-50 hover:text-gray-600",
+        secondary: "bg-gray-50 text-gray-900 hover:bg-gray-50",
+        ghost: "hover:text-gray-900 hover:text-gray-900",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "px-3 py-1 text-sm",
+        md: "px-4 py-2 text-base",
+        lg: "px-6 py-3 text-lg",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
